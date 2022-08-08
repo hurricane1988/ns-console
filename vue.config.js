30cab774b452f426fa6259e7e8521c25db76f375
hurricane1988 <hurricane_1988@163.com>
2022-08-08 21:52:26 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  devServer: {
+    host: "0.0.0.0", // 监听地址
+    port: 8080, // 监听端口
+    open: true // 启动后是否自动打开网页
+  },
+  transpileDependencies: true,
+  lintOnSave: false // 关闭语法检查
 })
